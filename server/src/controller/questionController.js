bd995734d6f816d8f4bb396241b73925a8e89b53
yupi <592789970@qq.com>
2022-07-27 22:55:53 +0800

    update 增加开发提示

--- server/src/controller/questionController.js
+++ server/src/controller/questionController.js
@@ -9,6 +9,7 @@ const _ = require("lodash");
  */
 async function listQuestionsApi(event, req, res) {
   const { size = 10 } = event;
+  // fixme 注意，如果是正式上线的答题程序，这里要把答案和各选项的分数信息抹掉
   return _.sampleSize(questions, size);
 }
 
